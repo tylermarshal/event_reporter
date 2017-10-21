879eecc51c9b6226043a84dcfe121da1c865ee5c
TMad <madsen.tyler@gmail.com>
2017-10-21 14:42:17 -0600

    add beginnings to shell

--- lib/event_reporter.rb
+++ lib/event_reporter.rb
@@ -1,6 +1,44 @@
 require 'pry'
+require './lib/queue'
+require './lib/help'
+require 'readline'
+require 'csv'
+
 
 class EventReporter
+
+  include 'Help'
+
+  stty_save = 'stty -g'.chomp
+
+  begin
+      while input = Readlines.readline("> ", true)
+        case input
+        when "exit"
+          break
+        when "load"
+          load
+        when "find"
+          find("query")
+        when "help"
+          Help.help
+        end
+      end
+    rescue Interrupt => e
+      system('stty', stty_save) #restore
+      exit
+  end
+
+
+  def system(command)
+    fork {
+      exec(command)
+    }
+  end
+
+
+
+  end
 
 
 end

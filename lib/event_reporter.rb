0d9901c3c6d599a821d188ba686ccfcbf1667ee1
TMad <madsen.tyler@gmail.com>
2017-10-22 11:01:35 -0600

    add case statements to shell

--- lib/event_reporter.rb
+++ lib/event_reporter.rb
@@ -19,4 +19,28 @@ class EventReporter
     end
   end
 
+  def command_router
+    if divided_input.length == 1
+      case divided_input.first
+      when "load"
+        load_main_file
+      when "help"
+        help_listing
+      end
+    else
+      case divided_input.first
+      when "load"
+        load_command(divided_input[1..-1])
+      when "help"
+        help_commands(divided_input[1..-1])
+      when "queue"
+        queue_commands(divided_input[1..-1])
+      when "find"
+        find_commands(divided_input[1..-1])
+      end
+    end
+  end
+
+
+
 end

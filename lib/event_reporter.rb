df8148400f1f9afa604f97a49c3094f0311b0d5a
TMad <madsen.tyler@gmail.com>
2017-10-22 10:25:30 -0600

    modify shell loop

--- lib/event_reporter.rb
+++ lib/event_reporter.rb
@@ -5,18 +5,18 @@ require './lib/load'
 require 'readline'
 require 'csv'
 
+class EventReporter
 
   def event_reporter
-
     until input == 'quit'
       input = gets.strip
       divided_input = input.split(' ')
       if divided_input == nil
         puts "Please enter a command"
-      elsif divided_input.length == 1
-        single_command(divided_input.first)
       else
-
-
+        command_router(divided_input)
+      end
     end
   end
+
+end

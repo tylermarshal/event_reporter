0298ecf5e85daceb75bbbb2631ac72f170d2f1b1
TMad <madsen.tyler@gmail.com>
2017-10-23 20:42:54 -0600

    add more queue items

--- test/help_test.rb
+++ test/help_test.rb
@@ -25,8 +25,33 @@ class HelpTest < Minitest::Test
   end
 
   def test_it_provides_queue_count_help
-    assert_equal "Populate the queue with all records matching the criteria for the given attribute. Format: find <attribute> <criteria>)",
-     help("help find")
+    assert_equal "Output how many records are in the current queue",
+     help("help queue count")
+  end
+
+  def test_it_provides_queue_clear_help
+    assert_equal "Empty the queue",
+     help("help queue clear")
+  end
+
+  def test_it_provides_queue_print_help
+    assert_equal "Print out a tab-delimited data table with a header row following this format: LAST NAME  FIRST NAME  EMAIL  ZIPCODE  CITY  STATE  ADDRESS  PHONE",
+    help("help queue print")
+  end
+
+  def test_it_provides_queue_print_by_help
+    assert_equal "Print the data table sorted by the specified attribute like zipcode. Format: queue print by <attribute>",
+     help("help queue print by")
+  end
+
+  def test_it_provides_queue_save_to_help
+    assert_equal "Export the current queue to the specified filename as a CSV. The file will include data and headers for last name, first name, email, zipcode, city, state, address, and phone number. (queue save to <filename.csv>)",
+     help("help queue save to")
+  end
+
+  def test_it_provides_queue_export_html_help
+    assert_equal "Export the current queue to the specified filename as a valid HTML file. Format: queue export html <filename.csv>",
+     help("help queue export html")
   end
 
 
--- test/queue_test.rb
+++ test/queue_test.rb
@@ -5,10 +5,11 @@ require 'pry'
 
 class QueueTest < Minitest::Test
 
-  def test_it_exists
-    queue = Queue.new
+  def test_it_prints
+    queue = [{:first_name=>"Black", :last_name=>"Jack", :email_address=>"asdfsdf@jumpstartlab.com", :homephone=>"1390838923", :street=>"1334 State St", :city=>"Minneapolis", :state=>"MN", :zipcode=>"55044"}, {:first_name=>"Minh", :last_name=>"Chadke", :email_address=>"uhswanso@jumpstartlab.com", :homephone=>"5189290000", :street=>"1 Chapin Way", :city=>"Northampton", :state=>"MA", :zipcode=>"01063"}, {:first_name=>"Jay", :last_name=>"Ray", :email_address=>"jay@jumpstartlab.com", :homephone=>"9522405682", :street=>"1042 Logan St", :city=>"Northampton", :state=>"MA", :zipcode=>"01064"}]
 
-    assert_instance_of Queue, queue
+
+
   end
 
 

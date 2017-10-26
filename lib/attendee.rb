e3dfe51c78ef2f83c7769e18c71c767463d694a8
TMad <madsen.tyler@gmail.com>
2017-10-25 19:13:54 -0600

    start attendee class'

--- lib/attendee.rb
+++ lib/attendee.rb
@@ -3,8 +3,11 @@ class Attendee
 
   attr_reader :clean_list
 
-  def initialize(clean_list)
-    @clean_list = []
+  def initialize(attendee)
+    @first_name = row[:first_name]
+    @last_name = row[:last_name]
   end
 
 end
+
+RegDate,first_Name,last_Name,Email_Address,HomePhone,Street,City,State,Zipcode
